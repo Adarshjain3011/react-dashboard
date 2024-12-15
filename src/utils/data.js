98e395783262ffd0bf55c89cdbb60c2c0415e6da
Adarshjain3011 <adarshjain3011@gamil.com>
2024-12-16 01:07:21 +0530

    update  tailwand config.js file

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,8 +1,8 @@
-import company from "../../public/icons/company-logo.png";
-import dashboard from "../../public/icons/dashboard-icon.png";
-import portfolio from "../../public/icons/portfolio-icon.png";
-import inputs from "../../public/icons/inputs-icon.png";
-import profile from "../../public/icons/profile-icon.png";
+import company from "/icons/company-logo.png";
+import dashboard from "/icons/dashboard-icon.png";
+import portfolio from "/icons/portfolio-icon.png";
+import inputs from "/icons/inputs-icon.png";
+import profile from "/icons/profile-icon.png";
 
 export const sideBarData = [
     {
