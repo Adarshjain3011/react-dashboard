f00d59e57cc3f0f214ec064fc18a734284e81431
Adarshjain3011 <adarshjain3011@gamil.com>
2024-12-16 03:21:08 +0530

    minor changes

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    // Shared build configurations for both frontend and backend
+    outDir: './frontend/dist',
+  },
+});
+
+
+
+
