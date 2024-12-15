c990c6c8320518a47bcbef93628692e3eec1b122
Adarshjain3011 <adarshjain3011@gamil.com>
2024-12-16 03:32:37 +0530

    minor changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../build', // Output directory
+    outDir: 'build', // Output directory
     emptyOutDir: true, // Clear the folder before building
   },
   server: {
